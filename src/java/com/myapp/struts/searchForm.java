70afa91a208a473c22efb73ef46da137a60d4f8d
JieYu <jyu43@crimson.ua.edu>
2017-04-13 02:41:47 -0500

    Search Page can do multi-criteria search e.g. actor + category.

---
+++ src/java/com/myapp/struts/searchForm.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myapp.struts;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionServlet;
+import org.apache.struts.upload.MultipartRequestHandler;
+
+/**
+ *
+ * @author jyu43
+ */
+public class searchForm extends ActionForm{
+
+    String filmId;
+    String category;
+    String actor;
+    String store;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public void setStore(String store) {
+        this.store = store;
+    }
+
+    public ActionServlet getServlet() {
+        return servlet;
+    }
+
+    public void setServlet(ActionServlet servlet) {
+        this.servlet = servlet;
+    }
+
+    public MultipartRequestHandler getMultipartRequestHandler() {
+        return multipartRequestHandler;
+    }
+
+    public void setMultipartRequestHandler(MultipartRequestHandler multipartRequestHandler) {
+        this.multipartRequestHandler = multipartRequestHandler;
+    }
+
+    public String getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(String filmId) {
+        this.filmId = filmId;
+    }
+
+}
---
+++ web/function.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function check() {
+    if(document.forms["my_form"]["text1"].value == "")
+        document.forms["my_form"]["text1"].value = "empty";
+}
+
